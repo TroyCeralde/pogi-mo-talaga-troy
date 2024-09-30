f2ccbcb2e63dbef6ea3a1c4847d22253c3aeb2d8
Troy Ceralde <117468396+TroyCeralde@users.noreply.github.com>
2024-10-01 01:55:50 +0800

    Add files via upload

---
+++ bago to/config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "I LOVE YOU!!!!",
+    introTitle: 'Can we go out?',
+    introDesc: `i know its so fast but can i get you a date? wrong grammar yan alam ko
+    pero pwede nga ba???`,
+    btnIntro: '^^CLICK THIS^^',
+    title: 'nice AHHAHA since you click it so can we??? 🥰',
+    desc: 'can wee?? ill promise ill make it worth itt ',
+    btnYes: 'yes',
+    btnNo: 'no',
+    question: 'oh ayan nagyes ka ha saan so payag na?? HAHA hindi kana makakapagtype riyan hayaan mo na yang whyy',
+    btnReply: 'click this!!!',
+    reply: '',
+    mess: 'promise hinding hindi ka magsisi and ill take my best shot',
+    messDesc: 'see you mwa take time to read it ',
+    btnAccept: 'chat na HAHAHA click this please make sure to play the music button ha',
+    messLink: 'https://troyceralde.github.io/pogimotroy/bago%20to%20part%202/index.html'
+};
